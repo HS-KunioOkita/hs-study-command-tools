f16f5fdca7afee6c7dfd5472899a9f88a4e950c4
hs_okita.kunio <okita.kunio@heartsoft.co.jp>
2022-08-18 09:46:33 +0900

    mock方法を変更

--- step03/src/test_unixteim2dateformat_simple.py
+++ step03/src/test_unixteim2dateformat_simple.py
@@ -1,13 +1,14 @@
-import pytest
 import sys
+from unittest.mock import patch
 
 from unixtime2dateformat_simple import main
 
+
 # 正常系確認
 def test_main(capfd, mocker):
-    test_argments = ['', '100']
-    mocker.patch('sys.argv', return_value=test_argments)
-    main()
-    out, err = capfd.readouterr()
-    assert out == '1970-01-01T09:00:01+0900\n'
-    assert err == ''
+    test_argments = ['', '1']
+    with patch.object(sys, 'argv', test_argments):
+        main()
+        out, err = capfd.readouterr()
+        assert out == '1970-01-01T09:00:01+0900\n'
+        assert err == ''
